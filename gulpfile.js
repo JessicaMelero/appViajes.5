416f27faf88e81655973a025cdb47be8ea32a544
Jessica Melero <jessicameleromartinez@MacBook-Pro-de-Jessica-4.local>
2018-11-13 17:29:10 +0100

    Modificado error de browser-sync por falta de w

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,33 @@
 var gulp = require('gulp');
-var uglify = require('uglify');
-var browserSync = require('broser-sync');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
 
-gulp.task('default', () => {
+//Ejecuta por defecto browser y min
+gulp.task("default", ['browser', 'min']);
+
+// Levanta el servidor
+gulp.task('browser', () => {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     })
 });
- 
+//  Minifica el proyecto
 gulp.task('min', () => {
     return gulp.src('js/app.js')
     .pipe(uglify)
     .pipe(gulp.dest('js/app.min.js'))
 });
 
+// Escucha si hay cambior en cualquier archivo dentro de js y con extension .js
 gulp.watch('js/**/*.js').on('change',function(){
     returngulp.src('js/app.js')
     .pipe(uglify())
     .pipe(gulp.dest('js/app.min.js'))
 });
 
-
+// Escucha si hay cambios en index.html y en app.js
 gulp.watch(['index.html', 'js/app.js']).on('change', () =>{
     browserSync.reload();
 });
