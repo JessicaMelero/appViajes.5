d7742d6db2ac1c6157c56ffe5cc68b9879b65efc
Jessica Melero <jessicameleromartinez@MacBook-Pro-de-Jessica-4.local>
2018-11-13 17:06:46 +0100

    Creando estructura de proyecto con Gulp y Bower inicializados

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var uglify = require('uglify');
+var browserSync = require('broser-sync');
+
+gulp.task('default', () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+});
+ 
+gulp.task('min', () => {
+    return gulp.src('js/app.js')
+    .pipe(uglify)
+    .pipe(gulp.dest('js/app.min.js'))
+});
